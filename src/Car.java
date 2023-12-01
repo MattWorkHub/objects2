9fac4ad1fd488e79582447c6e98aeba53e692281
MattWorkHub <150373956+MattWorkHub@users.noreply.github.com>
2023-12-01 15:48:25 +0100

    test

---
+++ src/Car.java
@@ -0,0 +1,49 @@
+public class Car {
+    public int cylinders;
+    public String plate;
+    public String brand;
+    public String model;
+
+    public Car(String brand, String model, int cylinders, String plate) {
+        this.cylinders = cylinders;
+        this.plate = plate;
+        this.brand = brand;
+        this.model = model;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public void setCylinders(int cylinders) {
+        this.cylinders = cylinders;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+
+    public void setPlate(String plate) {
+        this.plate = plate;
+    }
+
+    public String toString() {
+        return brand + " " + model + " " + cylinders + " " + plate;
+    }
+}
